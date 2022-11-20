94605893b4268b4bc9a4ddf06f1e6f4674f9b329
evgenia <eskholmanskaya@gmail.com>
2022-11-20 13:22:58 +0300

    Add Task09_2

--- Task09/Program.cs
+++ Task09/Program.cs
@@ -8,11 +8,27 @@
 int num = new Random().Next(10, 100);
 Console.WriteLine($"Случайное число из диапазона 10 - 99 -> {num}");
 
-int firstDigit = num / 10; //78 / 10 = 7
-int secondDigit = num % 10; //78 % 10 = 8
+// int firstDigit = num / 10; //78 / 10 = 7
+// int secondDigit = num % 10; //78 % 10 = 8
 
-if(firstDigit > secondDigit) Console.WriteLine($"Наибольшая цифра числа = {firstDigit}");
-else Console.WriteLine($"Наибольшая цифра числа = {secondDigit}");
+// if (firstDigit > secondDigit) Console.WriteLine($"Наибольшая цифра числа = {firstDigit}");
+// else Console.WriteLine($"Наибольшая цифра числа = {secondDigit}");
 
-int result = firstDigit > secondDigit ? firstDigit : secondDigit;
-Console.WriteLine($"Наибольшая цифра числа = {num} = {result}");
+// int result = firstDigit > secondDigit ? firstDigit : secondDigit;
+// Console.WriteLine($"Наибольшая цифра числа = {num} = {result}");
+
+// Console.Write($"Наибольшая цифра числа {num} = ");
+// Console.Write(firstDigit > secondDigit ? firstDigit : secondDigit);
+
+// int max = Math.Max(firstDigit, secondDigit);
+// Console.WriteLine($"Наибольшая цифра числа {num} = {max}");
+
+int MaxDigit(int num)
+{
+    int firstDigit = num / 10; //78 / 10 = 7
+    int secondDigit = num % 10; //78 % 10 = 8
+    return firstDigit > secondDigit ? firstDigit : secondDigit;
+}
+
+int maxDigit = MaxDigit(num);
+Console.WriteLine($"Наибольшая цифра числа = {num} = {maxDigit}");
